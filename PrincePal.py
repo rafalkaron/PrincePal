d45383327e918b9065879e65baa73092d7f460de
rafalkaron <rafalkaron@gmail.com>
2020-08-05 22:28:21 +0200

    redesigned the whole thing can select custom style now

--- PrincePal.py
+++ PrincePal.py
@@ -14,6 +14,7 @@ import time
 __version__ = "0.5"
 __author__ = "Rafał Karoń <rafalkaron@gmail.com>"
 
+
 def exe_dir():
     """Return the executable directory."""
     if getattr(sys, 'frozen', False):
@@ -33,26 +34,45 @@ def files_list(directory, files_extension):
         files_list = files_list_lowercase + files_list_uppercase
     return files_list
 
-def publish_pdf(source_file):
-    """Use the prince command to convert an HTML file to a PDF file and open the PDF file in the default application determined by OS."""    
-    os.system(f"prince \"{source_file}\"")
+def commands_list(directory, files_extension, output="", style=""):
+    """Return a list of files with a given extension in a directory."""
+    command_list = []
+    if os.name == "nt":     # Windows ignores file extension case. Getting uppercase and lowercase lists generates duplicates.
+        files_list = glob.glob(f"{directory}/*.{files_extension}")
+    if os.name == "posix":  # macOS does not ignore file extension case.
+        files_list_lowercase = glob.glob(f"{directory}/*.{files_extension.lower()}")
+        files_list_uppercase = glob.glob(f"{directory}/*.{files_extension.upper()}")
+        files_list = files_list_lowercase + files_list_uppercase
+    for file in files_list:
+        command_list.append(f"prince \"{file}\" {output} {style}")
+    return command_list
+
+def publish_pdf(command):
+    """Use the prince command to convert an HTML file to a PDF file and open the PDF file in the default application determined by OS."""        
+    os.system(command)
 
 def preview_pdf(source_file):
     webbrowser.open(url=f"file:///{source_file.lower().replace('.html', '.pdf')}", new=1, autoraise=False)
 
 def main():
-    sys.tracebacklimit = 0 # Disables traceback messages
+    #sys.tracebacklimit = 0 # Disables traceback messages
     par = argparse.ArgumentParser(description="Preview your PDFs like a prince!")
     par.add_argument("-v", "--version", action="version", version=f"%(prog)s {__version__}")
     par.add_argument("-rm", "--remove_pdfs", action="store_true", help="USE WITH CAUTION: Permanently remove PDF files from the script directory")
     par.add_argument("-nopr", "--no_preview", action="store_true", help="prevent PDFs from opening after publication")
-    par.add_argument("-nofb", "--no_feedback", action="store_true", help="hides non-critical feedback")
     par.add_argument("-yolo", "--you_live_only_once", action="store_true", help="combine with the '-rm' argument to permanently remove the PDF files from the script directory without confirmation.")
-    par.add_argument("-jobs", "--concurrent_jobs", metavar="jobs_number", help="determine the number of concurrent jobs (defults to 12)")
+    par.add_argument("-jobs", "--concurrent_jobs", metavar="jobs", help="determine the number of concurrent jobs (defults to 12)")
+    par.add_argument("-cwd", "--current_working_directory", action="store_true", help="Use HTML files in the script directory as an input")
+    par.add_argument("-i", "--input", metavar="source", help="Pick a source file or source folder on your own")
+    par.add_argument("-o", "--output", metavar="store_true", help="Pick the output folder on your own")
+    par.add_argument("-s", "--style", metavar="store_true", help="Pick the output folder on your own")
     args = par.parse_args()
     # Consider creating an if = true loop listening to any saves in the script directory/children directories. run script on save.
     # Add an exception that will terminate the script if no prince installation is found.
     # Consider adding custom input/output folder options
+    # Consider adding an option to close every web browser tab
+    
+    # Concurrent jobs settings
     if not args.concurrent_jobs:
         """The default number of concurrent jobs."""
         p = multiprocessing.Pool(12)
@@ -61,23 +81,8 @@ def main():
         jobs = int(args.concurrent_jobs)
         p = multiprocessing.Pool(jobs)
 
-    if not args.remove_pdfs:
-        """Publish and open PDFs."""
-        if not args.no_feedback:
-            start_time = time.time()
-        source_files = files_list(exe_dir(), "html")
-        if len(source_files) == 0:
-            raise Exception(f"No HTML files to convert in {exe_dir()}\nMove PrincePal to the directory with HTML files that you want to convert.")
-        p.map(publish_pdf, source_files)
-        if not args.no_feedback:
-            elapsed_time = time.time() - start_time
-            print(f"Converted {len(source_files)} HTML file(s) to PDFs in {round(elapsed_time, 3)} seconds.")
-        if not args.no_preview:
-            """Prevent PDFs from opening after publication."""
-            p.map(preview_pdf, source_files)
-        p.close()
-        p.join()
-    elif args.remove_pdfs:
+    # Remove PDFs feature
+    if args.remove_pdfs:
         """USE WITH CAUTION: Permanently remove PDF files from the script directory."""
         pdfs = files_list(exe_dir(), "pdf")
         pdfs_bullet_list = "\n * ".join([str(x) for x in pdfs])
@@ -99,6 +104,59 @@ def main():
             p.map(os.remove, pdfs)
             p.close()
             p.join()
+        sys.exit(0)
+
+    # Publishing
+    """Publish and open PDFs."""
+    #start_time = time.time()
+    
+    command_output = f" -o \"{args.output}\""
+    command_style = f" -s \"{args.style}\""
+
+    if args.current_working_directory:
+        p.map(publish_pdf, commands_list(exe_dir(), "html"))
+    
+    if args.input:
+        if os.path.isdir(args.input):
+            p.map(publish_pdf, commands_list(args.input, "html"))
+        elif os.path.isfile(args.input):
+            publish_pdf(f"prince \"{args.input}\"")
+    
+    if args.style:
+        print(commands_list(args.input, "html", style=command_style))
+        p.map(publish_pdf, commands_list(args.input, "html", style=command_style))
+
+    #elapsed_time = time.time() - start_time
+    #print(f"Converted {len(source)} HTML file(s) to PDFs in {round(elapsed_time, 3)} seconds.")
+    #if not args.no_preview:
+    #    p.map(preview_pdf, source)
+    p.close()
+    p.join()
+
+"""
+    if args.current_working_directory:
+        source = files_list(exe_dir(), "html")
+        p.map(publish_pdf, source)
+        if not args.no_preview:
+            p.map(preview_pdf, source)
+
+    elif args.input:
+        if os.path.isdir(args.input):
+            source = files_list(args.input, "html")
+            p.map(publish_pdf, source)
+            if not args.no_preview:
+                p.map(preview_pdf, source)
+
+        elif os.path.isfile(args.input):
+            source = args.input
+            publish_pdf(source)
+            if not args.no_preview:
+                preview_pdf(source)
+"""
+    #if len(source) == 0:
+    #    raise Exception(f"No HTML files to convert in {exe_dir()}\nMove PrincePal to the directory with HTML files that you want to convert.")
+    
+
 
 __main__ = os.path.basename(os.path.abspath(sys.argv[0])).replace(".py","") # The "__main__" name must be used in the if statement below because of multiprocessing.
 if __name__ == "__main__":
